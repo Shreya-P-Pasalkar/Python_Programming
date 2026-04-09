b23e3c3fc71aedd650b43def81d0506caa6b490c
Shreya-Pramod-Pasalkar <shreyappasalkar04@gmail.com>
2026-04-09 13:08:33 +0530

    Diabetes Case Study Prediction using KNN and Logistic Regression

--- Assignments/Assignment_49/DiabetesPrediction.py
+++ Assignments/Assignment_49/DiabetesPrediction.py
@@ -8,7 +8,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-#########################################################
+#########################################################################################
+#   Function Name : DisplayStart
+#   Description   : It is used to display header on console
+#   Input         : Info
+#   Output        : None
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def DisplayStart(Info) :
     Border = "="*80
 
@@ -16,13 +23,27 @@ def DisplayStart(Info) :
     print(Info)
     print(Border)
 
-#########################################################
+#########################################################################################
+#   Function Name : DisplayEnd
+#   Description   : It is used to display header on console
+#   Input         : None
+#   Output        : None
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def DisplayEnd() :
     Border = "="*80
 
     print(Border)
 
-#########################################################
+#########################################################################################
+#   Function Name : DataLoading
+#   Description   : It is used to load csv 
+#   Input         : Info
+#   Output        : None
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def DataLoading() :
     df = pd.read_csv("diabetes.csv")
 
@@ -30,7 +51,14 @@ def DataLoading() :
 
     return df
 
-#########################################################
+#########################################################################################
+#   Function Name : EDA
+#   Description   : It is used to perform Exploratory Data Analysis
+#   Input         : dataframe
+#   Output        : None
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def EDA(df) :
     print("----------------------------------------------------------")
     print("First 5 Rows from the Data : ")
@@ -105,7 +133,14 @@ def EDA(df) :
     
     print("----------------------------------------------------------")
 
-#########################################################
+#########################################################################################
+#   Function Name : DataPreprocessing
+#   Description   : It is used to perform Data Preprocessing
+#   Input         : Dataframe
+#   Output        : None
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def DataPreprocessing(df) :
     # Checking Missing Values
     print("----------------------------------------------------------")
@@ -137,7 +172,14 @@ def DataPreprocessing(df) :
     print("Features Scaled successfully!!")
     print("----------------------------------------------------------")
 
-#########################################################
+#########################################################################################
+#   Function Name : TrainTestSplit
+#   Description   : It is used to split dataset into traing and testing dataset
+#   Input         : Dataframe
+#   Output        : X_train, X_test, Y_train, Y_test
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def TrainTestSplit(df) : 
     X = df.drop(columns=["Outcome"])
     Y = df["Outcome"]
@@ -148,7 +190,14 @@ def TrainTestSplit(df) :
 
     return X_train, X_test, Y_train, Y_test
 
-#########################################################
+#########################################################################################
+#   Function Name : ModelBuilding
+#   Description   : It is used to built Machine Learning Model(KNN and Logistic Regression)
+#   Input         : X_train, Y_train
+#   Output        : KNNModel , LogisticModel
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def ModelBuilding(X_train, Y_train) :
     # Building Model for K-Nearest-Neighbors
     KNNModel = KNeighborsClassifier()
@@ -163,7 +212,14 @@ def ModelBuilding(X_train, Y_train) :
 
     return KNNModel , LogisticModel
 
-#########################################################
+#########################################################################################
+#   Function Name : ModelEvaluation
+#   Description   : It is used to evaluate Models
+#   Input         : KNNModel, LogisticModel, X_test, Y_test
+#   Output        : Y_pred1, Y_pred2
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def ModelEvaluation(KNNModel, LogisticModel, X_test, Y_test) :
     # KNN Model Evaluation
     print("----------------------------------------------------------")
@@ -219,7 +275,14 @@ def ModelEvaluation(KNNModel, LogisticModel, X_test, Y_test) :
 
     return Y_pred1, Y_pred2
 
-#########################################################
+#########################################################################################
+#   Function Name : FinalOutput
+#   Description   : It is used to create csv files
+#   Input         : KNNdf, Logisticdf
+#   Output        : None
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def FinalOutput(KNNdf, Logisticdf) :
     KNNdf.to_csv("KNNPrediction.csv")
     Logisticdf.to_csv("LogisticPrediction.csv")
@@ -227,7 +290,14 @@ def FinalOutput(KNNdf, Logisticdf) :
     print("KNN test results csv created successfully!!")
     print("Logistic Regression test results csv created successfully!!")
 
-#########################################################
+#########################################################################################
+#   Function Name : main
+#   Description   : Entry-point funtion
+#   Input         : None
+#   Output        : None
+#   Date          : 04/4/2026
+#   Author        : Shreya Pramod Pasalkar
+#########################################################################################
 def main() :
     DisplayStart("Step 1 : Data Loading")
     # Step 1 : Loading Data(csv)
@@ -269,6 +339,8 @@ def main() :
     
     DisplayEnd()
 
-#########################################################
+#########################################################################################
+#   Starter
+#########################################################################################
 if __name__ == "__main__" :
     main()
